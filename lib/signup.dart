758d2048263e9e8e8d4ce550d828ec65c3e0d5ac
AnanyaSinghal23 <singhalananya67@gmail.com>
2023-10-09 20:11:47 +0530

    Email Validation

--- lib/signup.dart
+++ lib/signup.dart
@@ -8,6 +8,7 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +19,9 @@ class _SignupPageState extends State<SignupPage> {
             child: Container(
               decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage('assets/image_vortex.jpg'),
-                    fit: BoxFit.cover,
-                  )
-              ),
+                image: AssetImage('assets/image_vortex.jpg'),
+                fit: BoxFit.cover,
+              )),
             ),
           ),
           Padding(
@@ -39,54 +39,86 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                   SizedBox(height: 20),
                   Form(
+                      key: _formKey,
                       child: Column(
                         children: [
                           TextFormField(
-                      style: TextStyle(
-                      color: Colors.blue[900], fontWeight: FontWeight.w800),
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.person),
-                          hintText: 'What do people call you?',
-                          labelText: 'Username *'),
-                    ),
-                    SizedBox(height: 10),
-                    TextFormField (
-                      style: TextStyle(
-                          color: Colors.blue[900], fontWeight: FontWeight.w800),
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.mail), labelText: 'Email *'),
-                      // onSaved: (String? value) {
-                      //
-                      // },
-                      validator: ((value) {
-                        if(value==null || value.isEmpty){
-                          return 'Please Enter Some Text';
-                        }
-                        return null;
-                      }),
-                    ),
-                    SizedBox(height: 10),
-                    TextFormField(
-                      style: TextStyle(
-                          color: Colors.blue[900], fontWeight: FontWeight.w800),
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.password), labelText: 'Password *'),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 30),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Sign Up'),
-                    ),
-                          TextButton(onPressed: (){
-                            Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => LoginPage()),
-                            );
-                          }, child: Text("Already have an account? Login"))
+                            style: TextStyle(
+                                color: Colors.blue[900],
+                                fontWeight: FontWeight.w800),
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.person),
+                                hintText: 'What do people call you?',
+                                labelText: 'Username *'),
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            style: TextStyle(
+                                color: Colors.blue[900],
+                                fontWeight: FontWeight.w800),
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.mail), labelText: 'Email *'),
+                            // onSaved: (String? value) {
+                            //
+                            // },
+                            // validator: ((value) {
+                            //   if(value==null || value.isEmpty){
+                            //     return 'Please Enter Some Text';
+                            //   }
+                            //   return null;
+                            // }),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              String pattern =
+                                  r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$';
+                              RegExp regex = RegExp(pattern);
+                              if (!regex.hasMatch(value)) {
+                                return 'Enter a valid email';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            style: TextStyle(
+                                color: Colors.blue[900],
+                                fontWeight: FontWeight.w800),
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.password),
+                                labelText: 'Password *'),
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 30),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    backgroundColor: Colors.indigo,
+                                      behavior: SnackBarBehavior.floating,
+                                      // showCloseIcon: true,
+                                      // closeIconColor: Colors.white,
+                                      dismissDirection: DismissDirection.startToEnd,
+                                      content: Text('Processing Data')),
+                                );
+                              }
+                            },
+                            child: Text('Sign Up'),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LoginPage()),
+                                );
+                              },
+                              child: Text("Already have an account? Login"))
                         ],
-                      )
-                  ),
-              ],
+                      )),
+                ],
               ),
             ),
           ),
